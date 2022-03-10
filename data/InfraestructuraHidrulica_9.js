65a9c2d682fe1b5836cb42cfc9c09b4392bfffb3
Hector Hugo Añamuro Luque <hugo.aluque@gmail.com>
2022-03-09 21:20:43 -0500

    update

---
+++ data/InfraestructuraHidrulica_9.js
@@ -0,0 +1,17 @@
+var json_InfraestructuraHidrulica_9 = {
+"type": "FeatureCollection",
+"name": "InfraestructuraHidrulica_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Infraestructura:": "Presa Lagunillas", "Datos :": "<a href='https://datastudio.google.com/u/2/reporting/3dc19c28-3194-448d-9bcd-fa0bc7b20a7a/page/qX5SC'target='blank'>Click Aqui</a> " }, "geometry": { "type": "Point", "coordinates": [ -70.704766298572693, -15.721325757143843 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Cabana - Mañazo", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.474557471474043, -15.661747074051544 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Captación Huataquita", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.409741513375906, -15.678977995408665 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Yocará", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.308213867390535, -15.598449065335094 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Huataquita", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.467965690589949, -15.657150106163062 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Cabanilla", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.388311683739175, -15.670910576534386 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Yanarico", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.336561575526076, -15.628925074514893 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Bocatoma Canteria", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.269714505794269, -15.559157064346195 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Captación Seda Juliaca", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.10625660329093, -15.46719303738441 ] } },
+{ "type": "Feature", "properties": { "Infraestructura:": "Partidor Rajo", "Datos :": "En Construcción" }, "geometry": { "type": "Point", "coordinates": [ -70.359653785285929, -15.706396918676161 ] } }
+]
+}
